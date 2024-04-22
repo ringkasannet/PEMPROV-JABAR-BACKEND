32e773775640f9c6ff4cd9a8f310dba86b224d25
ringkasan.net <ringkasan.net@gmail.com>
2024-04-22 20:05:54 +0700

    tambah tambah cari dokumen

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import express from 'express'
 import { dbClient } from './mongodb_handler.js'
+import { ObjectId } from 'mongodb';
 
 const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello !')
+  res.send('Hello ! YES')
 })
+
+app.get('/cekKesesuaian', (req, res) => {
+
+    //
+    res.send('Hello ! YES')
+  })
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
@@ -14,5 +21,10 @@ app.listen(port, () => {
 
 const collection = dbClient.db("pemprov-jabar-bumd").collection("BUMD");
 
-const findResult = await collection.find({}).toArray();
-console.log('Found documents =>', findResult);
+const objectId = new ObjectId("662650b65f0d70008a1ac6e2");
+
+const results = await collection.find({ _id: objectId }).toArray();
+console.log('Found documents =>', results[0].name); 
+
+//check all items in collection already embedded or not
+//if not pinecone 
