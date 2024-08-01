2ab0c42a030c6d3e58263a18e15e9848256c0589
ringkasan.net <ringkasan.net@gmail.com>
2024-08-01 15:14:09 +0700

    Update dataHandler.js

--- src/dataHandler.js
+++ src/dataHandler.js
@@ -109,7 +109,7 @@ async function getMatchesDocsAndPinecone(topFive){
 async function matchQueryToPinecone(queryValue,num){ 
   console.log('fungsi matchQueryToPinecone()');
   const embeddedQuery = await embeddingQuery(queryValue);
-  const matchingResults = await pc.matchVectorQuery(embeddedQuery,num+1);
+  const matchingResults = await pc.matchVectorQuery(embeddedQuery);
   const bumdList = await getMatchesDocsAndPinecone(matchingResults);
   return bumdList;
 };
